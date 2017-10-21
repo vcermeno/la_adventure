ff73a7f7b21065e88d3ff5e68e93f658ca1617a3
Vivian Cermeno <vcermeno6@gmail.com>
2017-10-21 16:37:19 -0700

    heroku fix

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,3 +1,7 @@
 class Adventure < ApplicationRecord
     mount_uploader :picture, PictureUploader
+    + geocoded_by :address
+    + after_validation :geocode          # this command auto fetches our coordinates
+      mount_uploader :picture, PictureUploader
+      has_many :comments
 end
