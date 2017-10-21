40bba6054dbd22d1e0e375ec644dd398995350dc
Vivian Cermeno <vcermeno6@gmail.com>
2017-10-21 16:06:34 -0700

    more bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021185311) do
+ActiveRecord::Schema.define(version: 20171021230432) do
 
   create_table "adventures", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20171021185311) do
     t.date "visit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
